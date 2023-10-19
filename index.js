90e629a11d50f8311c920611647c6a855731d4fb
Md-Arman01 <137868415+Md-Arman01@users.noreply.github.com>
2023-10-20 01:13:10 +0600

    delete specefic data from database

--- index.js
+++ index.js
@@ -90,6 +90,13 @@ async function run() {
       res.send(result);
 
     })
+    // delete
+    app.delete('/myCart/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await myCartCollection.deleteOne(query);
+      res.send(result)
+    })
 
 
     // -------
